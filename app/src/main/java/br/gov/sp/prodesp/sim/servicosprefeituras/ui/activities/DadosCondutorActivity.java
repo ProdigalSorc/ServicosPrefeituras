9dda41a902ebf508255ac26d0e4145cd4c113e0d
Rodrigo de Freitas Paixao <rpaixao@apoioprodesp.sp.gov.br>
2018-08-08 16:54:05 -0300

    Adicionando Dados do Condutor

---
+++ app/src/main/java/br/gov/sp/prodesp/sim/servicosprefeituras/ui/activities/DadosCondutorActivity.java
@@ -0,0 +1,15 @@
+package br.gov.sp.prodesp.sim.servicosprefeituras.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.gov.sp.prodesp.sim.servicosprefeituras.R;
+
+public class DadosCondutorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_condutor);
+    }
+}
