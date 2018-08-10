0be9e3adb4d09395ecbd5730498ef047caed24bc
Rodrigo de Freitas Paixao <rpaixao@apoioprodesp.sp.gov.br>
2018-08-10 08:53:03 -0300

    Adicionando novos componentes

--- app/src/main/java/br/gov/sp/prodesp/sim/servicosprefeituras/ui/fragments/TabProprietarioFragment.java
+++ app/src/main/java/br/gov/sp/prodesp/sim/servicosprefeituras/ui/fragments/TabProprietarioFragment.java
@@ -6,14 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 import br.gov.sp.prodesp.sim.servicosprefeituras.R;
+import br.gov.sp.prodesp.sim.servicosprefeituras.utils.DataUtil;
 
 public class TabProprietarioFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_proprietario, container, false);
+
+        TextView dataAtual = view.findViewById(R.id.activity_dados_proprietario_data);
+        dataAtual.setText(DataUtil.dataEmTexto(Calendar.getInstance()));
         return view;
     }
 }
