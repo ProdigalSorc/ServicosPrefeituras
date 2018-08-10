b7616105e65b32c439a187938f6c0f5e6d55af21
Rodrigo de Freitas Paixao <rpaixao@apoioprodesp.sp.gov.br>
2018-08-10 08:29:59 -0300

    Adicionando tab proprietário

--- app/src/main/java/br/gov/sp/prodesp/sim/servicosprefeituras/ui/fragments/TabProprietarioFragment.java
+++ app/src/main/java/br/gov/sp/prodesp/sim/servicosprefeituras/ui/fragments/TabProprietarioFragment.java
@@ -1,6 +1,19 @@
 package br.gov.sp.prodesp.sim.servicosprefeituras.ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.gov.sp.prodesp.sim.servicosprefeituras.R;
 
 public class TabProprietarioFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_proprietario, container, false);
+        return view;
+    }
 }
